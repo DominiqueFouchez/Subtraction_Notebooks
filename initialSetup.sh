5f791e3b29a04ec69f6c184e2c92ca63172c2a5d
Juan Pablo Reyes Gomez <jpreyes@marcosmo.in2p3.fr>
2015-02-12 10:46:02 +0100

    Subtraction script

--- initialSetup.sh
+++ initialSetup.sh
@@ -4,6 +4,11 @@ source ../v9_2_copy/loadLSST.sh
 setup obs_cfht
 setup pipe_tasks
 
+cd data/CFHTLS
+mkdir input
+echo 'lsst.obs.cfht.MegacamMapper' > input/_mapper
 
-eups declare -m none -r ../data astrometry_net_data CFHT-Deep
+cd ../..
+
+eups declare -force -m none -r data/astrometry_net_data/CFHT-Deep astrometry_net_data CFHT-Deep
 setup astrometry_net_data CFHT-Deep
