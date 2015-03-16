98c1a93875dc15e3f60825e0e99eaf5450130054
Juan Pablo Reyes Gomez <jpreyes@marcosmo.in2p3.fr>
2015-03-16 16:53:13 +0100

    test

--- initialSetup.sh
+++ initialSetup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 source ../v9_2_copy/loadLSST.sh
-setup obs_cfht
+
+#eups declare -force -m none -r ../my_packages/obs_cfht obs_cfht jp_obs_cfht
+setup obs_cfht jp_obs_cfht
 setup pipe_tasks
 
 cd data/CFHTLS
@@ -10,5 +12,5 @@ echo 'lsst.obs.cfht.MegacamMapper' > input/_mapper
 
 cd ../..
 
-eups declare -force -m none -r data/astrometry_net_data/CFHT-Deep astrometry_net_data CFHT-Deep
+#eups declare -force -m none -r data/astrometry_net_data/CFHT-Deep astrometry_net_data CFHT-Deep
 setup astrometry_net_data CFHT-Deep
