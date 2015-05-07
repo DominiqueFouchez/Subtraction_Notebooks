70c1462d6b5d9258f1a6b6f6a66281bddb97e303
Juan Pablo Reyes Gomez <jpreyes@marcosmo.in2p3.fr>
2015-05-07 18:39:35 +0200

    GOGO Gadget commit

--- Coadd.py
+++
@@ -1,6 +0,0 @@
-
-import lsst.pipe.tasks.ingest as Ingester
-
-
-IT = Ingester.IngestTask()
-IT.ingest("/renoir_data_00/jpreyes/data/raw/739518p.fits.fz","/renoir_data_00/jpreyes/data/raw/739518.p","copy")
