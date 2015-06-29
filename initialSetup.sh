05797abbe52af6019a20e56cf8e4bb9eef6310e8
Juan pablo Reyes <jreyes@ccage012.in2p3.fr>
2015-06-29 13:36:08 +0200

    update on 10.0

--- initialSetup.sh
+++ initialSetup.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-source ../v9_2_copy/loadLSST.sh
+source /sps/lsst/data/jreyes/stacks/stack_v10_0/loadLSST.bash
+export PYTHONPATH=$PYTHONPATH:/afs/in2p3.fr/home/j/jreyes/my_modules/
+export PATH=$PATH:/sps/lsst/Library/ds9.new/
 
-#eups declare -force -m none -r ../my_packages/obs_cfht obs_cfht jp_obs_cfht
-setup obs_cfht jp_obs_cfht
+setup obs_cfht
 setup pipe_tasks
-
-cd data/CFHTLS
-mkdir input
-echo 'lsst.obs.cfht.MegacamMapper' > input/_mapper
-
-cd ../..
-
-#eups declare -force -m none -r data/astrometry_net_data/CFHT-Deep astrometry_net_data CFHT-Deep
-setup astrometry_net_data CFHT-Deep
+setup astrometry_net_data CFHT_Deep
