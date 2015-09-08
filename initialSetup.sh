70ef034399b300a8dafe40b7dd0fd87ca0869653
Juan Pablo Reyes Gomez <jpreyes@marcosmo.in2p3.fr>
2015-09-08 15:45:06 +0200

    Becker subtraction method at 90%

--- initialSetup.sh
+++ initialSetup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-source ../v9_2_copy/loadLSST.sh
-
+#source ../v9_2_copy/loadLSST.sh
+source /renoir_data_00/fouchez/lsst/DM/stacks/v10.1/loadLSST.bash
 #eups declare -force -m none -r ../my_packages/obs_cfht obs_cfht jp_obs_cfht
-setup obs_cfht jp_obs_cfht
+#setup obs_cfht jp_obs_cfht
+setup obs_cfht DM-1593
 setup pipe_tasks
 
 cd data/CFHTLS
@@ -13,4 +14,4 @@ echo 'lsst.obs.cfht.MegacamMapper' > input/_mapper
 cd ../..
 
 #eups declare -force -m none -r data/astrometry_net_data/CFHT-Deep astrometry_net_data CFHT-Deep
-setup astrometry_net_data CFHT-Deep
+setup astrometry_net_data CFHT_Deep
